d0602634823bc7285dd9d2e16243619e8e168b20
Jozsef Ottucsak <ottucsakjozsef@gmail.com>
2020-12-08 22:40:56 +0100

    Closes #2 fill screen tiles

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from lib.Graphics import GameWindow
+from lib.Graphics import GameWindow, GameMap
+from lib.Utility import loadMap
 
+
+# Initialize tk
 gw = GameWindow(width=720, height=720)
+
+# Load map00
+map = GameMap(gw.canvas, loadMap("00.txt"))
+map.drawMap()
+
+gw.root.mainloop()
